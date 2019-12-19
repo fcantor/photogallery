6dfd2d906fa3a35777fa3844897a1c79b6a7e618
Francesca <fpcantor@gmail.com>
2019-12-18 16:15:21 -0800

    Create simple web server

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+// handlerFunc is a function that's called everytime someone comes to our web app;
+// runs any time a web request hits our server
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+// tells our server to start up and use the handlerFunc
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
